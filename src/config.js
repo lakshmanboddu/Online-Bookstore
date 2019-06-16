edccb382be4b41a93e123e140b3bbaa3b3c55ba8
lakshmanboddu <lakshmanboddu@github.com>
2019-06-15 20:06:45 -0400

    code for project

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    REGION: "us-east-1",
+    API_URL: "https://dhrjxf4cmd.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9F0IQggO9",
+    APP_CLIENT_ID: "64etpr46gfe26461n1o15f7fso",
+    IDENTITY_POOL_ID: "us-east-1:5d22edd6-ba18-4e60-87b0-39f6b4751078"
+  }
+};
